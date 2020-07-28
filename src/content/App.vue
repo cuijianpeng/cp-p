d119d671f6df4075a5247451207a80579e2d15dd
cuijianpeng <cuijianpeng@xuetangx.com>
2020-07-28 19:50:26 +0800

    调整

--- src/content/App.vue
+++ src/content/App.vue
@@ -61,51 +61,59 @@
         <el-step title="黄金任务"></el-step>
       </el-steps>
     </div>
-    <div style="margin: 48px auto 24px; padding: 0; width: 80%;">
-      <el-collapse accordion v-model="activeName">
-        <el-collapse-item title="自定义菜单" name="1">
-          <div style="margin: 12px 12px 0;">
-            <div style="clear: both; height: inherit; overflow: hidden; margin-bottom: 12px; line-height: 40px;">
+    <div class="cardlist">
+      <el-card shadow="hover">
+        <div class="elCardContent">
+          自定义菜单
+        </div>
+      </el-card>
+      <el-card shadow="hover">
+
+        <div slot="header" class="clearfix">
+          <div style="text-align: left;">
+            <span>设置自动回复</span>
+            <el-button size="mini" style="float: right; padding: 3px 0" type="text">关闭</el-button>
+          </div>
+        </div>
+        <div class="elCardContentDetail">
+          <div style="">
+            <div style="clear: both; height: inherit; overflow: hidden; margin-bottom: 12px;">
               <div style="float: right;">
-                <el-button type="primary" plain @click="visibleStatus.sidebar = true">任务讲解</el-button>
-                <el-button type="primary">已完成</el-button>
+                <el-button size="mini" @click="visibleStatus.sidebar = true">任务讲解</el-button>
+                <el-button size="mini">已完成</el-button>
               </div>
               被关注自动回复
-              <span style="font-size: 12px; color: #bfbfbf;">已有8940个学员完成此任务</span>
+              <div style="font-size: 12px; color: #bfbfbf;">已有8940个学员完成此任务</div>
             </div>
-            <div style="clear: both; height: inherit; overflow: hidden; margin-bottom: 12px; line-height: 40px;">
+            <div style="clear: both; height: inherit; overflow: hidden; margin-bottom: 12px;">
               <div style="float: right;">
-                <el-button type="primary" plain @click="visibleStatus.sidebar = true">任务讲解</el-button>
-                <el-button type="primary">已完成</el-button>
+                <el-button size="mini" @click="visibleStatus.sidebar = true">任务讲解</el-button>
+                <el-button size="mini">已完成</el-button>
               </div>
               收到消息自动回复
-              <span style="font-size: 12px; color: #bfbfbf;">已有7940个学员完成此任务</span>
+              <div style="font-size: 12px; color: #bfbfbf;">已有7940个学员完成此任务</div>
             </div>
-            <div style="clear: both; height: inherit; overflow: hidden; line-height: 40px;">
+            <div style="clear: both; height: inherit; overflow: hidden;">
               <div style="float: right;">
-                <el-button type="primary" plain @click="visibleStatus.sidebar = true">任务讲解</el-button>
-                <el-button type="primary">已完成</el-button>
+                <el-button size="mini" @click="visibleStatus.sidebar = true">任务讲解</el-button>
+                <el-button size="mini">已完成</el-button>
               </div>
               关键词自动回复
-              <span style="font-size: 12px; color: #bfbfbf;">已有5940个学员完成此任务</span>
+              <div style="font-size: 12px; color: #bfbfbf;">已有5940个学员完成此任务</div>
             </div>
           </div>
-        </el-collapse-item>
-        <el-collapse-item title="设置自动回复" disabled name="2">
-          <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-          <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-        </el-collapse-item>
-        <el-collapse-item title="发布一篇文章" disabled name="3">
-          <div>简化流程：设计简洁直观的操作流程；</div>
-          <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-          <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-        </el-collapse-item>
-        <el-collapse-item title="开启赞赏功能" disabled name="4">
-          <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-          <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
-        </el-collapse-item>
-      </el-collapse>
-
+        </div>
+      </el-card>
+      <el-card shadow="hover">
+        <div class="elCardContent">
+          发布一篇文章
+        </div>
+      </el-card>
+      <el-card shadow="hover">
+        <div class="elCardContent">
+          开启赞赏功能
+        </div>
+      </el-card>
     </div>
     <div style="margin: 0 auto 24px; padding: 48px 0; width: 80%; text-align: center;">
       <el-button type="primary" plain @click="submit">完成任务</el-button>
@@ -148,5 +156,27 @@ export default {
 <style lang="scss" scoped>
 .chrome-plugin-insertPage{
   text-align: left;
+
+  .cardlist{
+    text-align: center;
+
+    .elCardContent{
+      height: 180px;
+      width: 180px;
+      line-height: 180px;
+      font-size: 14px;
+    }
+    .elCardContentDetail{
+      min-width: 400px;
+      text-align: left;
+      // min-height: 200px;
+    }
+    .el-card{
+      display: inline-block;
+      margin: 0 12px;
+    }
+  }
+
+
 }
 </style>
